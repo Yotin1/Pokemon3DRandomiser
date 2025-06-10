142ea1b7fc00090e8da6097101dbc5a463f5d567
Yotin1 <58013144+Yotin1@users.noreply.github.com>
2025-06-10 17:23:07 +0100

    updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
-module io.github.yotin1 {
+module io.github.yotin1.p3drandomiser {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.prefs;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.io;
 
-    opens io.github.yotin1 to javafx.fxml;
-    exports io.github.yotin1;
+    opens io.github.yotin1.p3drandomiser.gui to javafx.fxml;
+
+    exports io.github.yotin1.p3drandomiser.gui;
 }
