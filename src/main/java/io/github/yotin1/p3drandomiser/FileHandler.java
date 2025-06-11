ffa72249cf2c90ab3ae8b240992c01f95b89f07c
Yotin1 <58013144+Yotin1@users.noreply.github.com>
2025-06-11 15:03:19 +0100

    added IOException handling

--- src/main/java/io/github/yotin1/p3drandomiser/FileHandler.java
+++ src/main/java/io/github/yotin1/p3drandomiser/FileHandler.java
@@ -3,12 +3,19 @@ package io.github.yotin1.p3drandomiser;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileHandler {
-    
-    public static List<String> readFile(String path) throws IOException {
-        
-        return Files.readAllLines(Paths.get(path));
+
+    public static List<String> readFile(String path) {
+
+        List<String> file = new ArrayList<>();
+        try {
+            file = Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return file;
     }
 }
--- src/main/java/io/github/yotin1/p3drandomiser/Randomiser.java
+++ src/main/java/io/github/yotin1/p3drandomiser/Randomiser.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +22,7 @@ public final class Randomiser {
     public Randomiser() {
     }
 
-    private static void setRandomRange(Set<String> generations) throws IOException {
+    private static void setRandomRange(Set<String> generations) {
 
         Set<String> ignoreSuffix = new HashSet<String>(
                 Set.of("mega", "mega_x", "mega_y", "heat", "fan", "mow", "wash", "frost", "unbound",
@@ -29,51 +30,54 @@ public final class Randomiser {
                         "therian", "sky", "zen"));
         Set<String> regions = new HashSet<String>(
                 Set.of("alola", "galar"));
-        Files.list(Paths.get(directory + "\\Content\\Pokemon\\Data"))
-                .filter(file -> !file.toFile().isDirectory())
-                .forEach(file -> {
-                    // System.out.println(file.getFileName().toString() + " - " +
-                    // StringUtils.getDigits(StringUtils.substringBefore(file.getFileName().toString(),
-                    // "_")));
-                    int num = Integer.parseInt(
-                            StringUtils.getDigits(StringUtils.substringBefore(file.getFileName().toString(), "_")));
-                    String suffix = StringUtils.substringBetween(file.getFileName().toString(), "_", ".dat");
-                    // suffixes.add(suffix);
-                    if (!ignoreSuffix.contains(suffix)) {
-                        if (!regions.contains(suffix) || generations.contains("regionalForms")) {
-                            if (1 <= num && num <= 151 && generations.contains("gen1")) {
-                                pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
-                            } else if (152 <= num && num <= 251 && generations.contains("gen2")) {
-                                pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
-                            } else if (252 <= num && num <= 386 && generations.contains("gen3")) {
-                                pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
-                            } else if (387 <= num && num <= 493 && generations.contains("gen4")) {
-                                pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
-                            } else if (494 <= num && num <= 649 && generations.contains("gen5")) {
-                                pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
-                            } else if (650 <= num && num <= 721 && generations.contains("gen6")) {
-                                pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
-                            } else if (722 <= num && num <= 809 && generations.contains("gen7")) {
-                                pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
-                            } else if (810 <= num && num <= 905 && generations.contains("gen8")) {
-                                pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+        try {
+            Files.list(Paths.get(directory + "\\Content\\Pokemon\\Data"))
+                    .filter(file -> !file.toFile().isDirectory())
+                    .forEach(file -> {
+                        // System.out.println(file.getFileName().toString() + " - " +
+                        // StringUtils.getDigits(StringUtils.substringBefore(file.getFileName().toString(),
+                        // "_")));
+                        int num = Integer.parseInt(
+                                StringUtils.getDigits(StringUtils.substringBefore(file.getFileName().toString(), "_")));
+                        String suffix = StringUtils.substringBetween(file.getFileName().toString(), "_", ".dat");
+                        // suffixes.add(suffix);
+                        if (!ignoreSuffix.contains(suffix)) {
+                            if (!regions.contains(suffix) || generations.contains("regionalForms")) {
+                                if (1 <= num && num <= 151 && generations.contains("gen1")) {
+                                    pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+                                } else if (152 <= num && num <= 251 && generations.contains("gen2")) {
+                                    pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+                                } else if (252 <= num && num <= 386 && generations.contains("gen3")) {
+                                    pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+                                } else if (387 <= num && num <= 493 && generations.contains("gen4")) {
+                                    pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+                                } else if (494 <= num && num <= 649 && generations.contains("gen5")) {
+                                    pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+                                } else if (650 <= num && num <= 721 && generations.contains("gen6")) {
+                                    pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+                                } else if (722 <= num && num <= 809 && generations.contains("gen7")) {
+                                    pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+                                } else if (810 <= num && num <= 905 && generations.contains("gen8")) {
+                                    pokeList.add(StringUtils.remove(file.getFileName().toString(), ".dat"));
+                                }
                             }
                         }
-                    }
-                });
+                    });
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 
-    private static Pokemon getRandPokemon() throws IOException {
-        return new Pokemon(pokeList.get(random.nextInt(pokeList.size())));
+    public static String getRandomPokemon() {
+        return pokeList.get(random.nextInt(pokeList.size()));
     }
 
-    public static void run() throws IOException {
+    public static void run() {
 
         setRandomRange(
-                new HashSet<>(Set.of("gen1", "gen2", "gen3", "gen4", "gen5", "gen6", "gen7", "gen8", "regionalForms")));
-        System.out.println(getRandPokemon().getName());
-
-        // .collect(Collectors.toList()));
+                new HashSet<>(Set.of("gen1", "gen2", "gen3", "gen4", "gen5", "gen6", "gen7",
+                        "gen8", "regionalForms")));
+        new WildMap("violet").getEncounters().forEach(e -> System.out.println(Arrays.toString(e)));
     }
 
     public static void run(Map<String, Boolean> checkBoxes, String gamemodeName, String seed) throws IOException {
