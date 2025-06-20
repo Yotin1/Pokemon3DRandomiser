27837283201a36e7fe9a0142d7562a71558bbe70
Yotin1 <58013144+Yotin1@users.noreply.github.com>
2025-06-20 19:23:10 +0100

    added mapfile as subclass to p3dfile

---
+++ src/main/java/io/github/yotin1/p3drandomiser/P3DMap.java
@@ -0,0 +1,27 @@
+package io.github.yotin1.p3drandomiser;
+
+import java.nio.file.Path;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class P3DMap extends P3DFile {
+
+    private String music;
+
+    public P3DMap(Path path) {
+        super(path);
+
+    }
+
+    public String findAttribute(int index, String key) {
+
+        return StringUtils.substringBetween(this.data.get(index), String.format("\"%s\"{", key), "}");
+    }
+
+    public void replaceAttribute(int index, String key, String newAttribute) {
+
+        String oldAttribute = findAttribute(index, key);
+        this.data.set(index, StringUtils.replace(this.data.get(index), String.format("\"%s\"{%s}", key, oldAttribute),
+                String.format("\"%s\"{%s}", key, newAttribute)));
+    }
+}
