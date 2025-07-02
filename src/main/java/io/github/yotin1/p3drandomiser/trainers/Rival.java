8cb945e94784e0a5d7d5b72619245f6a859494f7
Yotin1 <58013144+Yotin1@users.noreply.github.com>
2025-07-02 17:23:17 +0100

    class representing rival battle files

---
+++ src/main/java/io/github/yotin1/p3drandomiser/trainers/Rival.java
@@ -0,0 +1,10 @@
+package io.github.yotin1.p3drandomiser.trainers;
+
+import java.nio.file.Path;
+
+public class Rival extends Trainer {
+
+    public Rival(Path path) {
+        super(path);
+    }
+}
