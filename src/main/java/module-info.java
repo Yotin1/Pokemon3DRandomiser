f4050b92c94bd90322d3be36f072eee6e4219182
Yotin1 <58013144+Yotin1@users.noreply.github.com>
2025-07-16 18:03:29 +0100

    added Jackson dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,10 @@ module io.github.yotin1.p3drandomiser {
     requires java.prefs;
     requires org.apache.commons.lang3;
     requires org.apache.commons.io;
+    requires com.fasterxml.jackson.databind;
 
     opens io.github.yotin1.p3drandomiser.gui to javafx.fxml;
 
     exports io.github.yotin1.p3drandomiser.gui;
+    exports io.github.yotin1.p3drandomiser;
 }
